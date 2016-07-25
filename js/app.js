8bcbf9334641b4f5ca9b74b7fabdbddb2bc82e2b
Подивилов Евгений <evgeniy.podivilov@gmail.com>
2016-07-25 09:53:43 +0500

    Создал класс Tail для работы с плитками. метод render для перерисовки нового местоположения и значения плитки

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function Tail(options) {
+    this.element = document.createElement('div')
+    this.value = options.value;
+    this.position = options.position;
+}
+Tail.prototype.move = function (newPosition) {
+    this.position = newPosition;
+};
+Tail.prototype.render = function () {
+    const top = (this.position % 4) * 100,
+        left = (this.position / 4 |0) * 100
+    this.element.style.cssText = 'top: ' + top + 'px; left: ' + left + 'px;';
+    this.element.className = 'fade thing t' + this.value;
+    return this.element;
+}

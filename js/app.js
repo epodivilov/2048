0a840234c434b40f2e11b1f7b67e7c35f4c360b4
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2016-07-26 21:46:58 +0500

    Поправил именование плиток

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 function Tile(options) {
-    this.element = document.createElement('div')
+    this.element = document.createElement('div');
     this.value = options.value;
     this.position = {
         x: options.position.x,
         y: options.position.y
-    }
+    };
     this.isJoined = false;
 }
 Tile.prototype.move = function (newPosition) {
@@ -13,17 +13,17 @@ Tile.prototype.move = function (newPosition) {
 };
 Tile.prototype.render = function () {
     var top = this.position.y * 100,
-        left = this.position.x * 100
+        left = this.position.x * 100;
     this.element.style.cssText = 'top: ' + top + 'px; left: ' + left + 'px;';
     this.element.className = 'fade thing t' + this.value;
     this.isJoined = false;
     return this.element;
-}
+};
 
 var Game = (function () {
     var bestScore = localStorage.getItem('bestScore') || 0,
         gameScore = 0,
-        NewGame
+        NewGame;
 
     var playfield = document.getElementById('playfield'),
         gameover = document.getElementById('gameover'),
@@ -31,46 +31,46 @@ var Game = (function () {
         bestScoreEl = document.getElementById('bestscore');
 
     NewGame = function () {
-        this.tailList = [
+        this.tileList = [
             new Array(4),
             new Array(4),
             new Array(4),
             new Array(4)
         ]
-    }
+    };
 
-    NewGame.prototype.randomTail = function () {
+    NewGame.prototype.randomTile = function () {
         var randomPos = {
             x: Math.floor(Math.random() * 4),
             y: Math.floor(Math.random() * 4)
         };
 
-        while (this.tailList[randomPos.x][randomPos.y] !== undefined) {
+        while (this.tileList[randomPos.x][randomPos.y] !== undefined) {
             randomPos = {
                 x: Math.floor(Math.random() * 4),
                 y: Math.floor(Math.random() * 4)
             };
         }
 
-        this.tailList[randomPos.x][randomPos.y] = new Tile({
+        this.tileList[randomPos.x][randomPos.y] = new Tile({
             value: Math.random() * 10 > 9 ? 4 : 2,
             position: randomPos
-        })
-        playfield.appendChild(this.tailList[randomPos.x][randomPos.y].element)
-    }
+        });
+        playfield.appendChild(this.tileList[randomPos.x][randomPos.y].element)
+    };
 
-    NewGame.prototype.removeTail = function (tile) {
-        this.tailList[tile.position.x][tile.position.y] = undefined;
+    NewGame.prototype.removeTile = function (tile) {
+        this.tileList[tile.position.x][tile.position.y] = undefined;
         playfield.removeChild(tile.element);
-    }
+    };
 
     NewGame.prototype.moveAndJoin = function (tile, position) {
         tile.value *= 2;
         tile.isJoined = true;
-        this.removeTail(this.tailList[position.x][position.y]);
-        this.tailList[position.x][position.y] = tile;
+        this.removeTile(this.tileList[position.x][position.y]);
+        this.tileList[position.x][position.y] = tile;
         return tile.value;
-    }
+    };
 
     NewGame.prototype.__moveLeft = function (tile) {
         if (!tile || tile.position.x === 0) return;
@@ -83,23 +83,23 @@ var Game = (function () {
 
         do {
             newPos.x -= 1;
-        } while (this.tailList[newPos.x][newPos.y] === undefined && newPos.x > 0)
+        } while (this.tileList[newPos.x][newPos.y] === undefined && newPos.x > 0)
 
-        if (this.tailList[newPos.x][newPos.y] === undefined) {
-            this.tailList[newPos.x][newPos.y] = tile;
-        } else if (!this.tailList[newPos.x][newPos.y].isJoined && this.tailList[newPos.x][newPos.y].value === tile.value) {
+        if (this.tileList[newPos.x][newPos.y] === undefined) {
+            this.tileList[newPos.x][newPos.y] = tile;
+        } else if (!this.tileList[newPos.x][newPos.y].isJoined && this.tileList[newPos.x][newPos.y].value === tile.value) {
             reward = this.moveAndJoin(tile, newPos)
-        } else if (this.tailList[newPos.x+1][newPos.y] === undefined) {
-            this.tailList[++newPos.x][newPos.y] = tile;
+        } else if (this.tileList[newPos.x+1][newPos.y] === undefined) {
+            this.tileList[++newPos.x][newPos.y] = tile;
         } else {
             return;
         }
 
-        this.tailList[tile.position.x][tile.position.y] = undefined;
+        this.tileList[tile.position.x][tile.position.y] = undefined;
         tile.move(newPos);
 
         return reward;
-    }
+    };
     NewGame.prototype.__moveRight = function (tile) {
         if (!tile || tile.position.x === 3) return;
 
@@ -111,23 +111,23 @@ var Game = (function () {
 
         do {
             newPos.x += 1;
-        } while (this.tailList[newPos.x][newPos.y] === undefined && newPos.x < 3)
+        } while (this.tileList[newPos.x][newPos.y] === undefined && newPos.x < 3)
 
-        if (this.tailList[newPos.x][newPos.y] === undefined) {
-            this.tailList[newPos.x][newPos.y] = tile;
-        } else if (!this.tailList[newPos.x][newPos.y].isJoined && this.tailList[newPos.x][newPos.y].value === tile.value) {
+        if (this.tileList[newPos.x][newPos.y] === undefined) {
+            this.tileList[newPos.x][newPos.y] = tile;
+        } else if (!this.tileList[newPos.x][newPos.y].isJoined && this.tileList[newPos.x][newPos.y].value === tile.value) {
             reward = this.moveAndJoin(tile, newPos)
-        } else if (this.tailList[newPos.x-1][newPos.y] === undefined) {
-            this.tailList[--newPos.x][newPos.y] = tile;
+        } else if (this.tileList[newPos.x-1][newPos.y] === undefined) {
+            this.tileList[--newPos.x][newPos.y] = tile;
         } else {
             return;
         }
 
-        this.tailList[tile.position.x][tile.position.y] = undefined;
+        this.tileList[tile.position.x][tile.position.y] = undefined;
         tile.move(newPos);
 
         return reward;
-    }
+    };
     NewGame.prototype.__moveUp = function (tile) {
         if (!tile || tile.position.y === 0) return;
 
@@ -139,23 +139,23 @@ var Game = (function () {
 
         do {
             newPos.y -= 1;
-        } while (this.tailList[newPos.x][newPos.y] === undefined && newPos.y > 0)
+        } while (this.tileList[newPos.x][newPos.y] === undefined && newPos.y > 0)
 
-        if (this.tailList[newPos.x][newPos.y] === undefined) {
-            this.tailList[newPos.x][newPos.y] = tile;
-        } else if (!this.tailList[newPos.x][newPos.y].isJoined && this.tailList[newPos.x][newPos.y].value === tile.value) {
+        if (this.tileList[newPos.x][newPos.y] === undefined) {
+            this.tileList[newPos.x][newPos.y] = tile;
+        } else if (!this.tileList[newPos.x][newPos.y].isJoined && this.tileList[newPos.x][newPos.y].value === tile.value) {
             reward = this.moveAndJoin(tile, newPos)
-        } else if (this.tailList[newPos.x][newPos.y+1] === undefined) {
-            this.tailList[newPos.x][++newPos.y] = tile;
+        } else if (this.tileList[newPos.x][newPos.y+1] === undefined) {
+            this.tileList[newPos.x][++newPos.y] = tile;
         } else {
             return;
         }
 
-        this.tailList[tile.position.x][tile.position.y] = undefined;
+        this.tileList[tile.position.x][tile.position.y] = undefined;
         tile.move(newPos);
 
         return reward;
-    }
+    };
     NewGame.prototype.__moveDown = function (tile) {
         if (!tile || tile.position.y === 3) return;
 
@@ -167,24 +167,24 @@ var Game = (function () {
 
         do {
             newPos.y += 1;
-        } while (this.tailList[newPos.x][newPos.y] === undefined && newPos.y < 3)
+        } while (this.tileList[newPos.x][newPos.y] === undefined && newPos.y < 3);
 
-        if (this.tailList[newPos.x][newPos.y] === undefined) {
-            this.tailList[newPos.x][newPos.y] = tile;
-        } else if (!this.tailList[newPos.x][newPos.y].isJoined && this.tailList[newPos.x][newPos.y].value === tile.value) {
+        if (this.tileList[newPos.x][newPos.y] === undefined) {
+            this.tileList[newPos.x][newPos.y] = tile;
+        } else if (!this.tileList[newPos.x][newPos.y].isJoined && this.tileList[newPos.x][newPos.y].value === tile.value) {
             reward = this.moveAndJoin(tile, newPos)
-        } else if (this.tailList[newPos.x][newPos.y-1] === undefined) {
-            this.tailList[newPos.x][--newPos.y] = tile;
+        } else if (this.tileList[newPos.x][newPos.y-1] === undefined) {
+            this.tileList[newPos.x][--newPos.y] = tile;
         } else {
             return;
         }
 
-        this.tailList[tile.position.x][tile.position.y] = undefined;
+        this.tileList[tile.position.x][tile.position.y] = undefined;
         tile.move(newPos);
 
         return reward;
-    }
-    NewGame.prototype.moveTails = function (direction) {
+    };
+    NewGame.prototype.moveTiles = function (direction) {
         var score = 0;
         var isMoved = false;
 
@@ -192,16 +192,16 @@ var Game = (function () {
             for (var row = 0; row < 4; row++) {
                 switch (direction) {
                     case 'left':
-                        score = this.__moveLeft(this.tailList[column][row]);
+                        score = this.__moveLeft(this.tileList[column][row]);
                         break;
                     case 'right':
-                        score = this.__moveRight(this.tailList[3 - column][row]);
+                        score = this.__moveRight(this.tileList[3 - column][row]);
                         break;
                     case 'up':
-                        score = this.__moveUp(this.tailList[column][row]);
+                        score = this.__moveUp(this.tileList[column][row]);
                         break;
                     case 'down':
-                        score = this.__moveDown(this.tailList[column][3 - row]);
+                        score = this.__moveDown(this.tileList[column][3 - row]);
                         break;
                     default: return;
                 }
@@ -214,19 +214,19 @@ var Game = (function () {
         }
 
         if (isMoved) {
-            this.randomTail();
+            this.randomTile();
             if (this.checkGameOver()) gameover.style.zIndex = '100';
         }
-    }
+    };
 
     NewGame.prototype.checkGameOver = function () {
         var isGameOver = false;
         for (var column = 0; column < 4; column++) {
             for (var row = 0; row < 4; row++) {
 
-                var curentTile = this.tailList[column][row],
-                    rightTile = column < 3 ? this.tailList[column+1][row] : undefined,
-                    bottomTile = row < 3 ? this.tailList[column][row+1] : undefined;
+                var curentTile = this.tileList[column][row],
+                    rightTile = column < 3 ? this.tileList[column+1][row] : undefined,
+                    bottomTile = row < 3 ? this.tileList[column][row+1] : undefined;
 
                 if (curentTile === undefined) {
                     return false;
@@ -244,20 +244,20 @@ var Game = (function () {
             }
         }
         return isGameOver;
-    }
+    };
 
     NewGame.prototype.reset = function () {
         var self = this;
-        this.tailList.filter(Boolean).forEach(function(column) {
+        this.tileList.filter(Boolean).forEach(function(column) {
             column.filter(Boolean).forEach(function (tile) {
-                self.removeTail(tile);
+                self.removeTile(tile);
             })
-        })
+        });
         gameover.style.zIndex = '-1';
         gameScore = 0;
-        this.randomTail();
+        this.randomTile();
         this.render();
-    }
+    };
 
     NewGame.prototype.render = function () {
         bestScore = gameScore > bestScore ? gameScore : bestScore;
@@ -265,15 +265,15 @@ var Game = (function () {
         bestScoreEl.textContent = bestScore;
         localStorage.setItem('bestScore', bestScore);
 
-        this.tailList.filter(Boolean).forEach(function(column) {
+        this.tileList.filter(Boolean).forEach(function(column) {
             column.filter(Boolean).forEach(function (tile) {
                 tile.render()
             })
         })
-    }
+    };
 
     return NewGame;
-})()
+})();
 
 function Mouse() {
     this.downX = 0;
@@ -298,15 +298,15 @@ Mouse.prototype.getDirection = function () {
     }
 
     return direction;
-}
+};
 Mouse.prototype.onMouseDown = function (event) {
     this.downX = event.pageX;
     this.downY = event.pageY;
-}
+};
 Mouse.prototype.onMouseUp = function (event) {
     this.upX = event.pageX;
     this.upY = event.pageY;
-}
+};
 
 
 window.onload = function () {
@@ -320,39 +320,39 @@ window.onload = function () {
 
     document.addEventListener('keydown', function (e) {
         switch (e.which) {
-            case 37: game.moveTails('left');  break;
-            case 38: game.moveTails('up');    break;
-            case 39: game.moveTails('right'); break;
-            case 40: game.moveTails('down');  break;
+            case 37: game.moveTiles('left');  break;
+            case 38: game.moveTiles('up');    break;
+            case 39: game.moveTiles('right'); break;
+            case 40: game.moveTiles('down');  break;
             default: return;
         }
 
         game.render();
-    })
+    });
 
     playfield.addEventListener('mousedown', function (e) {
         mouse.onMouseDown(e)
-    })
+    });
 
     playfield.addEventListener('touchstart', function (e) {
         var touchEvent = e.touches[0];
         mouse.onMouseDown(touchEvent);
-    })
+    });
 
     playfield.addEventListener('mouseup', function (e) {
-        mouse.onMouseUp(e)
-        game.moveTails(mouse.getDirection());
+        mouse.onMouseUp(e);
+        game.moveTiles(mouse.getDirection());
         game.render();
-    })
+    });
 
     playfield.addEventListener('touchend', function (e) {
         var touchEvent = e.changedTouches[0];
-        mouse.onMouseUp(touchEvent)
-        game.moveTails(mouse.getDirection());
+        mouse.onMouseUp(touchEvent);
+        game.moveTiles(mouse.getDirection());
         game.render();
-    })
+    });
 
     resetBtn.addEventListener('click', function () {
         game.reset()
     })
-}
+};

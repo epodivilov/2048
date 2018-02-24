bda2ac0c181163e5253409d220ba86248ce05137
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2018-02-24 21:45:43 +0500

    Updates

---
+++ swCache.js
@@ -0,0 +1,51 @@
+const VERSION = 0.2;
+const CACHE_NAME = `swCache_v_${VERSION}`;
+
+function fromCache (request) {
+  return caches.open(CACHE_NAME).then((cache) =>
+    cache.match(request).then((matching) =>
+      matching || Promise.reject('no-match')
+    ));
+}
+
+function update (request) {
+  return caches.open(CACHE_NAME).then((cache) =>
+    fetch(request).then((response) =>
+      cache.put(request, response)
+    )
+  );
+}
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    new Promise(resolve => {
+      self.addEventListener('message', ({ data: { cacheUrls } }) => {
+        caches.open(CACHE_NAME)
+          .then(cache => cache.addAll(cacheUrls))
+          .then(resolve);
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith('swCache_v_')) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request));
+});
+
